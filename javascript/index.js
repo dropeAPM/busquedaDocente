25939bf0a429d12aa6f3676ac5229ffc0530412c
dropeAPM <131218651+dropeAPM@users.noreply.github.com>
2024-07-07 23:26:00 -0400

    se modificaron las funciones de la busqueda añadiendo si ocurre una busqueda vacia se mostrara todos los docentes registrados

--- javascript/index.js
+++ javascript/index.js
@@ -2,18 +2,22 @@ function buscarDocente() {
     var busqueda = document.getElementById("busq").value.toLowerCase();
     var listaDocentes = JSON.parse(localStorage.getItem("listaDocentes")) || [];
 
-    var resultado = listaDocentes.filter(function(docente) {
-        return docente.rut.toLowerCase().includes(busqueda) || 
-               docente.nombre.toLowerCase().includes(busqueda);
-    });
-
-    if (resultado.length === 1) {
-        localStorage.setItem("docenteEncontrado", JSON.stringify(resultado[0]));
-        window.location.href = "html/docente.html";
-    } else if (resultado.length > 1) {
-        alert("Se encontraron múltiples coincidencias. Por favor, sea más específico.");
+    if (busqueda === "") {
+        localStorage.setItem("docenteEncontrado", JSON.stringify(listaDocentes));
+        window.location.href = "detalle_docente.html";
     } else {
-        alert("No se encontraron coincidencias.");
+        var resultado = listaDocentes.filter(function(docente) {
+            return docente.rut.toLowerCase().includes(busqueda) || 
+                   docente.nombre.toLowerCase().includes(busqueda);
+        });
+
+        if (resultado.length === 1) {
+            localStorage.setItem("docenteEncontrado", JSON.stringify(resultado[0]));
+            window.location.href = "detalle_docente.html";
+        } else if (resultado.length > 1) {
+            alert("Se encontraron múltiples coincidencias. Por favor, sea más específico.");
+        } else {
+            alert("No se encontraron coincidencias.");
+        }
     }
 }
-
